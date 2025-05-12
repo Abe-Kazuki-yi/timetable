e112cb855af264041b7884007b3b18f21a17114e
DESKTOP-RQTQL0J\student <ericangyue@gmail.com>
2025-05-12 17:41:56 +0900

    secondStepの変更

--- src/main/java/com/management/timetable/controller/StudentHomeController.java
+++ src/main/java/com/management/timetable/controller/StudentHomeController.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/student")
 public class StudentHomeController {
+	
 	@GetMapping
-	public String showLogin() {
+	public String showStudentHome() {
 		return "admin/student/studentHome";
+	}
+	
+	@GetMapping("/register")
+	public String showStudentRegister() {
+		return "admin/student/studentRegister";
+	}
+	
+	@GetMapping("/account")
+	public String showStudentAccount() {
+		return "admin/student/studentAccount";
+	}
+	
+	@GetMapping("/schedule")
+	public String showStudentSchedule() {
+		return "admin/student/studentSchedule";
 	}
 }
